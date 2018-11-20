5e22647e28aa85013c80b9f8124e82525c932393
William Song <30965609+Freakwill@users.noreply.github.com>
2018-11-20 10:26:18 +0800

    Add files via upload

---
+++ english.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Transform English to Logic Expressions
+
+Example:
+>>> res = english2logic('John isa Thing')
+>>> print(res)
+isa(John, Thing)
+"""
+
+import types
+
+import pyparsing as pp
+import pyparsing_ext as ppx
+
+
+class SentenceAction(ppx.BaseAction):
+    def __init__(self, instring='', loc=0, tokens=[]):
+        super(SentenceAction, self).__init__(instring, loc, tokens)
+        self.relation = tokens.relation
+        self.object = tokens.object
+        self.subject = tokens.subject
+
+class GeneralQuestionAction(SentenceAction):
+
+    def __str__(self):
+        if self.has('relation') and self.relation:
+            return '%s(%s, %s)?' % (self.relation, self.subject, self.object)
+        else:
+            return '%s(%s)?' % (self.object, self.subject)
+
+    def exec(self, memory):
+        if self.has('relation') and self.relation:
+            return memory[self.object] in getattr(memory[self.subject], self.relation).indirect()
+        else:
+            return isinstance(memory[self.subject], memory[self.object])
+
+class SpecialQuestionAction(SentenceAction):
+
+    def __str__(self):
+        return '%s(%s, %s)?' % (self.relation, self.subject, self.object)
+
+    def exec(self, memory):
+        if self.object in {'Who', 'What'}:
+            return ', '.join(a.name for a in getattr(memory[self.subject], self.relation).indirect())
+        elif self.subject in {'Who', 'What'}:
+            return ','.join(getattr(getattr(memory[self.subject], self.relation), 'inverse').indirect())
+
+        
+class StatementAction(SentenceAction):
+    def __init__(self, instring='', loc=0, tokens=[]):
+        super(StatementAction, self).__init__(instring, loc, tokens)
+        if self.has('explain'):
+            self.explain = tokens.explain
+
+    def __str__(self):
+        if self.has('explain') and self.explain:
+            return '%s and %s(%s, %s)' % (self.explain, self.relation, self.subject, self.object)
+        else:
+            return '%s(%s, %s)' % (self.relation, self.subject, self.object)
+
+    def exec(self, memory):
+        if self.relation == 'is a':
+            memory.update({self.subject: memory[self.object](self.subject)})
+        elif self.relation == 'sort':
+            memory.update({self.subject: types.new_class(self.subject, (self.object,))})
+        else:
+            memory.update({self.relation: types.new_class(self.relation, (type(memory[self.subject]) >> type(memory[self.object]),))})
+            getattr(memory[self.subject], self.relation).extend([memory[self.object]])
+
+
+Noun = pp.Word(pp.alphanums)
+Verb = pp.Word(pp.alphas)
+
+Phrase = pp.Literal('a') + (Noun('relation') + 'of' + Noun('object') | Noun('object'))
+GeneralQuestion = (pp.CaselessLiteral('Is') + Noun('subject') + Phrase \
+| pp.CaselessLiteral('Dose') + Noun('subject') + Verb('relation') + Noun('object')) + '?'
+
+SpecialQuestion = (pp.CaselessLiteral('What') | pp.CaselessLiteral('Who'))('object') + 'dose' + Noun('subject') + Verb('relation') \
+    | (pp.CaselessLiteral('Who')('subject') + Verb('relation') + Noun('object') \
+    | pp.CaselessLiteral('Which') + Noun('object') + 'dose' + Noun('subject') + Verb('relation')) + '?'
+Question = SpecialQuestion.addParseAction(SpecialQuestionAction) | GeneralQuestion.addParseAction(GeneralQuestionAction)
+
+Statement = pp.Forward()
+Statement <<= Noun('subject') + (('is a' + Noun('relation') + 'of') | pp.Literal('is a')('relation') | Verb('relation')) + Noun('object') + pp.Optional(pp.Keyword('where') + Statement('explain'))
+Statement.addParseAction(StatementAction)
+
+Sentence = Question | Statement
+
+
+def english2logic(s):
+    return Sentence.parseString(s)[0]
+
+
---
+++ owlgimbiseo.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pathlib
+
+from owlready2 import *
+
+import english
+
+
+PATH = pathlib.Path("database")
+onto_path.append(PATH)
+gimbiseo = get_ontology("http://test.org/gimbiseo.owl")
+
+
+def parse(s):
+    return english.english2logic(s)
+
+
+def answer(q, memory):
+    print('I am thinking...')
+    # locals().update(memory)
+    sync_reasoner(debug=0)
+    return q.exec(memory)
+
+
+def ask(q, memory):
+    if q.subject not in memory:
+        print('-- What is %s?' % (q.subject))
+    elif q.object not in memory:
+        print('-- What is %s?' % (q.object))
+    elif q.relation not in memory and q.relation != 'isa':
+        print('-- What is %s?' % (q.relation))
+
+
+with gimbiseo:
+    memory = locals()
+    while True:
+        q = parse(input('-- '))
+        if isinstance(q, english.StatementAction):
+            q.exec(memory)
+            print('-- I get.')
+        elif isinstance(q, english.GeneralQuestionAction):
+            try:
+                a = answer(q, memory)
+                if a:
+                    print('-- Yes')
+                else:
+                    print('-- No')
+            except Exception:
+                ask(q, memory)
+        else:
+            try:
+                a = answer(q, memory)
+                if a:
+                    print('-- %s' % str(a))
+                else:
+                    print('-- I don\'t know')
+            except Exception:
+                ask(q, memory)
+
+
