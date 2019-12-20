529d768fe64d776d5b284d891f84fffa0ff89b7b
William Song <30965609+Freakwill@users.noreply.github.com>
2019-12-20 20:05:57 +0800

    Add files via upload

--- gimbiseo/ui.py
+++ gimbiseo/ui.py
@@ -1,39 +1,72 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import time, random, types
+import time, random, types, pathlib
+import yaml
 
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
 from PyQt5.QtGui import QTextCursor, QIcon, QPixmap
 from PyQt5.QtCore import QBasicTimer, QDateTime
-from window import *
 
-from main import *
-from view import *
+from gimbiseo.window import *
+from gimbiseo.dialogue import *
 
 
-class DialogueUI(QMainWindow, Ui_Dialog):
+VIEW_PATH = pathlib.Path('view.yaml')
+view = yaml.load(VIEW_PATH.read_text())
+for k, v in view.items():
+    globals()[k]= v
+
+memory = ChineseMemory()
+
+class DialogueUI(QMainWindow, Ui_MainWindow):
     """UI for Dialogue
     """
     def __init__(self, dialogue, parent=None):
         super(DialogueUI, self).__init__(parent)
         self.setupUi(self)
+        self.more_setup()
+        self.dialogue = dialogue
+
+    def more_setup(self):
+        qss = pathlib.Path('style.qss')
+        if not qss.exists():
+            qss = pathlib.Path(__file__).parent / qss
+        self.setStyleSheet(qss.read_text())
         self.button_save.pressed.connect(self.save)
         self.button_quit.pressed.connect(self.quit)
         self.button_demo.pressed.connect(self.demo)
         self.button_load.pressed.connect(self.load)
         self.button_clear.pressed.connect(self.clear)
         self.edit_input.returnPressed.connect(self.submit)
-        self.label_logo.setPixmap(QPixmap('images/logo.png'))
-        self.dialogue = dialogue
+
+        self.actionLoad_L.triggered.connect(self.load)
+        self.actionSave_S.triggered.connect(self.save)
+        self.actionSave_As.triggered.connect(self.save_as)
+        self.actionClose_W.triggered.connect(self.close)
+        self.actionDocument.triggered.connect(self.open_document)
+        self.actionHome.triggered.connect(self.open_home)
+
+        logo = pathlib.Path('images/logo.png')
+        if not logo.exists():
+            logo = pathlib.Path(__file__).parent / logo
+        self.label_logo.setPixmap(QPixmap(str(logo)))
 
     def load(self):
-        self.display('目前不支持载入功能')
-        # self.dialogue.base=get_ontology("file:///home/jiba/onto/pizza_onto.owl").load()
+        fname, _ = QFileDialog.getOpenFileName(self, '加载知识库', str(pathlib.Path(__file__).parent / 'kb'), '*.owl')
+        if fname:
+            self.dialogue.base=get_ontology(fname).load()
+            self.inform(f'加载知识库{fname}')
 
 
     def save(self):
         self.dialogue.base.save()
+        self.inform('知识库存储完毕')
+
+    def save_as(self):
+        fname, _ = QFileDialog.getOpenFileName(self, '保存知识库', str(pathlib.Path(__file__).parent / 'kb'), '*.owl')
+        self.dialogue.base.save(fname)
+        self.inform('知识库存储完毕')
 
     def clear(self):
         self.text_dialogue.clear()
@@ -41,7 +74,15 @@ class DialogueUI(QMainWindow, Ui_Dialog):
     def quit(self):
         if False:
             self.save()
-        self.close()
+        memory.reset()
+        self.inform('退出对话')
+
+    def open_document(self):
+        subprocess.run(['open', pathlib.Path(__file__).parent / 'helpdoc.pdf'])
+
+    def open_home(self):
+        import webbrowser
+        webbrowser.open("https://github.com/Freakwill/gimbiseo")
 
     def submit(self):
         q = self.edit_input.text()
@@ -61,7 +102,7 @@ class DialogueUI(QMainWindow, Ui_Dialog):
         #     for i in t:
         #         self.text_dialogue.insertPlainText(i)
         # self.display = types.MethodType(typing, self)
-        from qadict import testy
+        from .qadict import testy
         self.test = iter(testy)
         self.q = self.r = None
         self.text_information.setPlainText(f'3秒钟后开始演示')
@@ -71,14 +112,15 @@ class DialogueUI(QMainWindow, Ui_Dialog):
         if self.timer.isActive():
             self.timer.stop()
         else:
-            self.timer.start(150, self)
-            self.text_information.setPlainText(f'演示开始于{QDateTime.currentDateTime().toString("yyyy-MM-dd hh:mm:ss")}')
+            self.timer.start(180, self)
+            self.inform(f'演示开始于{QDateTime.currentDateTime().toString("yyyy-MM-dd hh:mm:ss")}')
 
     def display(self, t, p=None):
         if p is None:
             self.text_dialogue.append(f'<div><em>{t}</em></div>')
         else:
-            self.text_dialogue.append(f'''<div><img src="{p['path']}" height=18 width=18 alt="{p['emoji']}"/>: {t}</div>''')
+            self.text_dialogue.append(f'''<div><img src="{p['path']}" height=18 width=18 alt="{p['emoji']}"/>: 
+                <span class="{p['class']}">{t}</span></div>''')
 
     def timerEvent(self, e):
         def _submit():
@@ -100,7 +142,7 @@ class DialogueUI(QMainWindow, Ui_Dialog):
             except:
                 self.timer.stop()
                 self.display('演示结束')
-                self.text_information.setPlainText(f'演示结束于{QDateTime.currentDateTime().toString("yyyy-MM-dd hh:mm:ss")}')
+                self.inform(f'演示结束于{QDateTime.currentDateTime().toString("yyyy-MM-dd hh:mm:ss")}')
         elif self.r is None:
             try:
                 if random.random()>0.5:
@@ -113,12 +155,17 @@ class DialogueUI(QMainWindow, Ui_Dialog):
             _reply(self._q)
             self.r = self.q = None
 
+    def inform(self, s):
+        self.text_information.setPlainText(s)
 
-if __name__ == '__main__':
-    memory = ChineseMemory()
-    d = Dialogue()
-    with d.base:
-        app = QApplication([])
-        myWin=DialogueUI(d)
-        myWin.show()
-        app.exec_()
+
+# if __name__ == '__main__':
+#     import sys
+#     memory = ChineseMemory()
+#     d = Dialogue()
+#     with d.base:
+#         app = QApplication(sys.argv)
+#         dui=DialogueUI(d)
+#         dui.show()
+#         os.system("pause")
+#         sys.exit(app.exec_())
